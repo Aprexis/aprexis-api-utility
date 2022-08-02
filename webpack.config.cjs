ba3c9574ddd6819f88f3c778fbdf28827a11334c
Ian Brown <ibrown@bear-code.com>
2022-08-02 12:37:58 -0400

    Continue trying to get the module to build properly.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,20 +4,22 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
+        exclude: /(node_modules|bower_components|dist)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['env'],
+            plugins: ['transform-object-rest-spread']
           }
         }
       }, {
