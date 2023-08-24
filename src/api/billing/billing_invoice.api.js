9961af87ea4d1d0ebf0a01c0f97a3a8b59e5acb5
Ian Brown <ibrown@bear-code.com>
2023-08-24 14:18:40 -0400

    Added billing invoices support.

---
+++ src/api/billing/billing_invoice.api.js
@@ -0,0 +1,62 @@
+import { API } from "../"
+
+export const billingInvoiceApi = {
+  index,
+  indexForHealthPlan,
+  list,
+  listForHealthPlan,
+  profile,
+  show
+}
+
+function index(credentials, params, onSuccess, onFailure) {
+  const method = 'GET'
+  const path = '/billing/invoices'
+  API.perform(method, path, API.buildQueryString(params), credentials, onSuccess, onFailure)
+}
+
+function indexForHealthPlan(credentials, health_plan_id, params, onSuccess, onFailure) {
+  if (!API.validateId('health plan ID', health_plan_id, onFailure)) {
+    return
+  }
+
+  const method = 'GET'
+  const path = `/health_plans/${health_plan_id}/billing/invoices`
+  API.perform(method, path, API.buildQueryString(params), credentials, undefined, onSuccess, onFailure)
+}
+
+function list(credentials, params, onSuccess, onFailure) {
+  const method = 'GET'
+  const path = '/billing/invoices/list'
+  API.perform(method, path, API.buildQueryString(params), credentials, onSuccess, onFailure)
+}
+
+function listForHealthPlan(credentials, health_plan_id, params, onSuccess, onFailure) {
+  if (!API.validateId('health plan ID', health_plan_id, onFailure)) {
+    return
+  }
+
+  const method = 'GET'
+  const path = `/health_plans/${health_plan_id}/billing/invoices/list`
+  API.perform(method, path, API.buildQueryString(params), credentials, undefined, onSuccess, onFailure)
+}
+
+function profile(credentials, id, onSuccess, onFailure) {
+  if (!API.validateId('invoice ID', id, onFailure)) {
+    return
+  }
+
+  const method = 'GET'
+  const path = `/billing/invoices/${id}/profile`
+  API.perform(method, path, '', credentials, undefined, onSuccess, onFailure)
+}
+
+function show(credentials, id, onSuccess, onFailure) {
+  if (!API.validateId('invoice ID', id, onFailure)) {
+    return
+  }
+
+  const method = 'GET'
+  const path = `/billing/invoices/${id}`
+  API.perform(method, path, '', credentials, undefined, onSuccess, onFailure)
+}
---
+++ src/helpers/billing/billing_invoice.helper.js
@@ -0,0 +1,67 @@
+import { fieldHelper, healthPlanHelper, idHelper, modelDatesHelper } from "../"
+
+export const billingInvoiceHelper = {
+  ...idHelper,
+  ...modelDatesHelper,
+  amount,
+  amountPaid,
+  canDelete,
+  canEdit,
+  dueAt,
+  healthPlan,
+  healthPlanCode,
+  healthPlanId,
+  healthPlanName,
+  name,
+  resolvedAt,
+  submittedAt
+}
+
+function amount(billingInvoice) {
+  return fieldHelper.getField(billingInvoice, "amount")
+}
+
+function amountPaid(billingInvoice) {
+  return fieldHelper.getField(billingInvoice, "amount_paid")
+}
+
+function canDelete(_user, _billingInvoice) {
+  return false
+}
+
+function canEdit(_user, _billingInvoice) {
+  return false
+}
+
+
+function dueAt(billingInvoice) {
+  return fieldHelper.getField(billingInvoice, "due_at")
+}
+
+function healthPlan(billingInvoice) {
+  return fieldHelper.getField(billingInvoice, "health_plan")
+}
+
+function healthPlanCode(billingInvoice) {
+  return healthPlanHelper.code(billingInvoiceHelper.healthPlan(billingInvoice))
+}
+
+function healthPlanId(billingInvoice) {
+  return idHelper.associatedId(billingInvoice, "health_plan", billingInvoiceHelper)
+}
+
+function healthPlanName(billingInvoice) {
+  return healthPlanHelper.name(billingInvoiceHelper.healthPlan(billingInvoice))
+}
+
+function name(biillingInvoice) {
+  return fieldHelper.getField(biillingInvoice, "name")
+}
+
+function resolvedAt(billingInvoice) {
+  return fieldHelper.getField(billingInvoice, "resolved_at")
+}
+
+function submittedAt(billingInvoice) {
+  return fieldHelper.getField(billingInvoice, "submitted_at")
+}
