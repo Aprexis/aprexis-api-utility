86c25b38d3e63ef45c3fbe6e69a162bdf4bef855
Ian Brown <ibrown@bear-code.com>
2025-02-07 12:00:55 -0500

    Added Gold Standard maintenance medications API and helper. (#18)

---
+++ src/api/gold_standard/gold_standard_maintenance_medication.api.js
@@ -0,0 +1,40 @@
+import { API } from '../'
+
+export const goldStandardMaintenanceMedicationApi = {
+  index,
+  list,
+  profile,
+  show
+}
+
+function index(credentials, params, onSuccess, onFailure) {
+  const method = 'GET'
+  const path = '/gold_standard/maintenance_medications'
+  API.perform(method, path, API.buildQueryString(params), credentials, undefined, onSuccess, onFailure)
+}
+
+function list(credentials, params, onSuccess, onFailure) {
+  const method = 'GET'
+  const path = '/gold_standard/maintenance_medications/list'
+  API.perform(method, path, API.buildQueryString(params), credentials, undefined, onSuccess, onFailure)
+}
+
+function profile(credentials, id, onSuccess, onFailure) {
+  if (!API.validateId('package ID', id, onFailure)) {
+    return
+  }
+
+  const method = 'GET'
+  const path = `/gold_standard/maintenance_medications/${id}/profile`
+  API.perform(method, path, '', credentials, undefined, onSuccess, onFailure)
+}
+
+function show(credentials, id, onSuccess, onFailure) {
+  if (!API.validateId('package ID', id, onFailure)) {
+    return
+  }
+
+  const method = 'GET'
+  const path = `/gold_standard/maintenance_medications/${id}`
+  API.perform(method, path, '', credentials, undefined, onSuccess, onFailure)
+}
---
+++ src/helpers/gold_standard/gold_standard_maintenance_medication.helper.js
@@ -0,0 +1,124 @@
+import { fieldHelper, idHelper, modelDatesHelper } from '../'
+import { goldStandardSpecificProductHelper } from "./gold_standard_specific_product.helper"
+import { goldStandardMarketedProductHelper } from './gold_standard_marketed_product.helper'
+import { goldStandardPackageHelper } from './gold_standard_package.helper'
+import { goldStandardProductHelper } from './gold_standard_product.helper'
+
+export const goldStandardMaintenanceMedicationHelper = {
+  ...modelDatesHelper,
+  canDelete,
+  canEdit,
+  goldStandardMarketedProduct,
+  goldStandardMarketedProductId,
+  goldStandardMarketedProductName,
+  goldStandardPackage,
+  goldStandardPackageDescription,
+  goldStandardPackageId,
+  goldStandardProduct,
+  goldStandardProductId,
+  goldStandardProductNameLong,
+  goldStandardProductNameShort,
+  goldStandardSpecificProduct,
+  goldStandardSpecificProductId,
+  goldStandardSpecificProductName,
+  ndc11
+}
+
+function canDelete(_user, _goldStandardMaintenanceMedication) {
+  return false
+}
+
+function canEdit(_user, _goldStandardMaintenanceMedication) {
+  return false
+}
+
+function goldStandardMarketedProduct(goldStandardMaintenanceMedication) {
+  return fieldHelper.getField(goldStandardMaintenanceMedication, "marketed_product")
+}
+
+function goldStandardMarketedProductId(goldStandardMaintenanceMedication) {
+  return idHelper.associatedId(
+    goldStandardMaintenanceMedication,
+    'marketed_product',
+    goldStandardMaintenanceMedicationHelper,
+    "goldStandardMarketedProduct",
+    "marketed_product_id"
+  )
+}
+
+function goldStandardMarketedProductName(goldStandardMaintenanceMedication) {
+  return goldStandardMarketedProductHelper.name(
+    goldStandardMaintenanceMedicationHelper.goldStandardMarketedProduct(goldStandardMaintenanceMedication)
+  )
+}
+function goldStandardPackage(goldStandardMaintenanceMedication) {
+  return fieldHelper.getField(goldStandardMaintenanceMedication, 'package')
+}
+
+function goldStandardPackageDescription(goldStandardMaintenanceMedication) {
+  return goldStandardPackageHelper.packageDescription(
+    goldStandardMaintenanceMedicationHelper.goldStandardPackage(goldStandardMaintenanceMedication)
+  )
+}
+
+function goldStandardPackageId(goldStandardMaintenanceMedication) {
+  return idHelper.associatedId(
+    goldStandardMaintenanceMedication,
+    'package',
+    goldStandardMaintenanceMedicationHelper,
+    "goldStandardPackage",
+    "package_id"
+  )
+}
+
+function goldStandardProduct(goldStandardMaintenanceMedication) {
+  return fieldHelper.getField(goldStandardMaintenanceMedication, 'product')
+}
+
+function goldStandardProductId(goldStandardMaintenanceMedication) {
+  return idHelper.associatedId(
+    goldStandardMaintenanceMedication,
+    'product',
+    goldStandardMaintenanceMedicationHelper,
+    "goldStandardProduct",
+    "product_id"
+  )
+}
+
+function goldStandardProductNameLong(goldStandardMaintenanceMedication) {
+  return goldStandardProductHelper.productNameLong(
+    goldStandardMaintenanceMedicationHelper.goldStandardProduct(goldStandardMaintenanceMedication)
+  )
+}
+
+function goldStandardProductNameShort(goldStandardMaintenanceMedication) {
+  return goldStandardProductHelper.productNameShort(
+    goldStandardMaintenanceMedicationHelper.goldStandardProduct(goldStandardMaintenanceMedication)
+  )
+}
+
+function goldStandardSpecificProduct(goldStandardMaintenanceMedication) {
+  return fieldHelper.getField(goldStandardMaintenanceMedication, "specific_product")
+}
+
+function goldStandardSpecificProductId(goldStandardMaintenanceMedication) {
+  return idHelper.associatedId(
+    goldStandardMaintenanceMedication,
+    'specific_product',
+    goldStandardMaintenanceMedicationHelper,
+    "goldStandardSpecificProduct",
+    "specific_product_id"
+  )
+}
+
+function goldStandardSpecificProductName(goldStandardMaintenanceMedication) {
+  return goldStandardSpecificProductHelper.name(
+    goldStandardMaintenanceMedicationHelper.goldStandardSpecificProduct(goldStandardMaintenanceMedication)
+  )
+}
+
+function ndc11(goldStandardMaintenanceMedication) {
+  return goldStandardPackageHelper.ndc11(
+    goldStandardMaintenanceMedicationHelper.goldStandardPackage(goldStandardMaintenanceMedication)
+  )
+}
